a309270c636f7616d92f8a9d49f546db13d78319
GongDolHoon <qkrtmdgns23@gmail.com>
2020-10-28 00:01:05 +0900

    Modify Camera Class

--- GongDolHoon/OpenGlSample/camera.h
+++ GongDolHoon/OpenGlSample/camera.h
@@ -5,108 +5,101 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
-namespace gdh_engine {
-	namespace object {
-		enum class CameraMovement;
+namespace object {
+	enum class CameraMovement
+	{
+		kForward = 0,
+		kBackward,
+		kLeft,
+		kRight,
+	};
 
-		class Camera
+	class Camera
+	{
+	#pragma region SINGLETON_PATTERN
+	public:
+		static Camera* get_instance()
 		{
-		public:
-			void ProcessKeyboard(CameraMovement direction, float delta_time);
-			void ProcessMouseMovement(float x_offset, float y_offset,
-				bool constrain_pitch = true);
-			void ProcessMouseScroll(float y_offset);
+			if (instance_ == nullptr)
+			{
+				instance_ = new Camera();
+			}
+			return instance_;
+		}
 
-			static Camera* get_instance()
-			{
-				if (instance_ == nullptr)
-				{
-					instance_ = new Camera();
-				}
+		~Camera() {}
 
-				return instance_;
-			}
+	private:
+		Camera(glm::vec3 position = glm::vec3(0.f, 0.f, 0.f),
+			glm::vec3 up = glm::vec3(0.f, 0.f, -10.f), float yaw = -90.f, float pitch = 0.f);
+		Camera(float pos_x, float pos_y, float pos_z,
+			float up_x, float up_y, float up_z, float yaw, float pitch);
+		static Camera* instance_;
+	#pragma endregion
+	public:
+		inline glm::mat4 get_view_matrix() const;
+		inline glm::vec3 get_position() const;
+		inline glm::vec3 get_front() const;
+		inline glm::vec3 get_up() const;
+		inline glm::vec3 get_right() const;
+		inline glm::vec3 world_up() const;
+		inline float get_yaw() const;
+		inline float get_pitch() const;
+		inline float get_zoom() const;
+		inline void set_camera_position(glm::vec3 target_position);
+	private:
+		void UpdateCameraVectors();
 
-			glm::mat4 get_view_matrix()
-			{
-				return glm::lookAt(position_, position_ + front_, up_);
-			}
-			glm::vec3 get_position() const
-			{
-				return position_;
-			}
-			glm::vec3 get_front() const
-			{
-				return front_;
-			}
-			glm::vec3 get_up() const
-			{
-				return up_;
-			}
-			glm::vec3 get_right() const
-			{
-				return right_;
-			}
-			glm::vec3 world_up() const
-			{
-				return world_up_;
-			}
-			float get_yaw() const
-			{
-				return yaw_;
-			}
-			float get_pitch() const
-			{
-				return pitch_;
-			}
-			float get_movement_speed() const
-			{
-				return movement_speed_;
-			}
-			float get_movement_sensitivity() const
-			{
-				return mouse_sensitivity_;
-			}
-			float get_zoom() const
-			{
-				return zoom_;
-			}
+		glm::vec3 position_;
+		glm::vec3 front_;
+		glm::vec3 up_;
+		glm::vec3 right_;
+		glm::vec3 world_up_;
 
-			void set_camera_position(glm::vec3 target_position)
-			{
-				position_ = target_position;
-			}
-		private:
-			Camera(glm::vec3 position = glm::vec3(0.f, 0.f, 0.f),
-				glm::vec3 up = glm::vec3(0.f, 0.f, -10.f), float yaw = -90.f, float pitch = 0.f);
-			Camera(float pos_x, float pos_y, float pos_z,
-				float up_x, float up_y, float up_z, float yaw, float pitch);
-			~Camera() {}
+		float yaw_;
+		float pitch_;
+		float zoom_;
+	};
 
-			static Camera* instance_;
-
-			void UpdateCameraVectors();
-
-			glm::vec3 position_;
-			glm::vec3 front_;
-			glm::vec3 up_;
-			glm::vec3 right_;
-			glm::vec3 world_up_;
-
-			float yaw_;
-			float pitch_;
-			float movement_speed_;
-			float mouse_sensitivity_;
-			float zoom_;
-		};
-
-		enum class CameraMovement
-		{
-			kForward = 0,
-			kBackward,
-			kLeft,
-			kRight,
-		};
-	}	// namespace object
-}	// namespace gdh_engine
+	glm::mat4 Camera::get_view_matrix() const
+	{
+		return glm::lookAt(position_, position_ + front_, up_);
+	}
+	glm::vec3 Camera::get_position() const
+	{
+		return position_;
+	}
+	glm::vec3 Camera::get_front() const
+	{
+		return front_;
+	}
+	glm::vec3 Camera::get_up() const
+	{
+		return up_;
+	}
+	glm::vec3 Camera::get_right() const
+	{
+		return right_;
+	}
+	glm::vec3 Camera::world_up() const
+	{
+		return world_up_;
+	}
+	float Camera::get_yaw() const
+	{
+		return yaw_;
+	}
+	float Camera::get_pitch() const
+	{
+		return pitch_;
+	}
+	float Camera::get_zoom() const
+	{
+		return zoom_;
+	}
+	void Camera::set_camera_position(glm::vec3 target_position)
+	{
+		position_ = target_position;
+	}
+}	// namespace object
 #endif // GDH_ENGINE_CAMERA_H
