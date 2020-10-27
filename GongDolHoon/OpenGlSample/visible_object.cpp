bbf3ddd4cc35dee1886bf0bfc9ecf84e52079e61
GongDolHoon <qkrtmdgns23@gmail.com>
2020-10-27 23:33:53 +0900

    Modify Visible Object Class

--- GongDolHoon/OpenGlSample/visible_object.cpp
+++ GongDolHoon/OpenGlSample/visible_object.cpp
@@ -8,162 +8,61 @@
 #include "texture.h"
 #include "transform.h"
 
-namespace gdh_engine {
-	namespace object {
+
+namespace object {
+	using namespace component;
 #pragma region Constructor & Destructor
-		VisibleObject::VisibleObject(const char* vertex_path, const char* fragment_path, const char* texture_path,
-			const char* obj_path, ObjTransInform target_transform,
-			TextureType dimensional_inform, TextureType alpha)
-		{
-			object_shader_ = new Shader(vertex_path, fragment_path);
-			object_texture_ = new Texture(texture_path, dimensional_inform, alpha);
-			object_mesh_ = new Mesh(obj_path);
-			object_transform_ = new Transform(target_transform);
+	VisibleObject::VisibleObject(LoaderParams* params)
+		:InterfaceObject(params)
+	{
+		object_shader_ = 
+			new Shader(params->get_vertex_path(), params->get_fragment_path());
+		object_texture_ = 
+			new Texture(params->get_texture_path()
+			, params->IsTexture3D() 
+			, params->IsTextureAlpha());
+		object_mesh_ = new Mesh(params->get_obj_path());
+		object_transform_ = 
+			new Transform(params->get_trans_inform());
 
-			object_mesh_->SetUpMesh();
-			SetTextureUniformToShader("texture1", 0);
-		}
-		VisibleObject::VisibleObject(std::string vertex_path, std::string fragment_path, std::string texture_path,
-			const char* obj_path, ObjTransInform target_transform,
-			TextureType dimensional_inform, TextureType alpha)
-		{
-			object_shader_ = new Shader(vertex_path, fragment_path);
-			object_texture_ = new Texture(texture_path, dimensional_inform, alpha);
-			object_mesh_ = new Mesh(obj_path);
-			object_transform_ = new Transform(target_transform);
-
-			object_mesh_->SetUpMesh();
-			SetTextureUniformToShader("texture1", 0);
-		}
-		VisibleObject::VisibleObject(const char* vertex_path, const char* fragment_path, const char* texture_path,
-			const char* obj_path, TextureType dimensional_inform, TextureType alpha)
-		{
-			object_shader_ = new Shader(vertex_path, fragment_path);
-			object_texture_ = new Texture(texture_path, dimensional_inform, alpha);
-			object_mesh_ = new Mesh(obj_path);
-			object_transform_ = new Transform();
-
-			object_mesh_->SetUpMesh();
-			SetTextureUniformToShader("texture1", 0);
-		}
-		VisibleObject::VisibleObject(std::string vertex_path, const char* fragment_path, const char* texture_path,
-			const char* obj_path, TextureType dimensional_inform, TextureType alpha)
-		{
-			object_shader_ = new Shader(vertex_path, fragment_path);
-			object_texture_ = new Texture(texture_path, dimensional_inform, alpha);
-			object_mesh_ = new Mesh(obj_path);
-			object_transform_ = new Transform();
-
-			object_mesh_->SetUpMesh();
-			SetTextureUniformToShader("texture1", 0);
-		}
-	
-		VisibleObject::~VisibleObject()
-		{
-			delete object_shader_;
-			delete object_texture_;
-			delete object_mesh_;
-		}
+		object_mesh_->SetUpMesh();
+		SetTextureUniformToShader("texture1", 0);
+	}
+	VisibleObject::~VisibleObject()
+	{
+		delete object_shader_;
+		delete object_texture_;
+		delete object_mesh_;
+		delete object_transform_;
+	}
 #pragma endregion
-		void VisibleObject::Update()
-		{
+	void VisibleObject::Update()
+	{
 
-		}
-	
-		void VisibleObject::SetupMesh()
-		{
-			object_mesh_->SetUpMesh();
-		}
-		void VisibleObject::SetTextureUniformToShader(std::string tex_uniform_name, unsigned int tex_num)
-		{
-			object_shader_->UseShader();
-			object_shader_->SetIntegerDataTypeUniformVariable(tex_uniform_name, tex_num);
-		}
-		void VisibleObject::SetTextureUniformToShader(const char* tex_uniform_name, unsigned int tex_num)
-		{
-			object_shader_->UseShader();
-			object_shader_->SetIntegerDataTypeUniformVariable(tex_uniform_name, tex_num);
-		}
-		void VisibleObject::ActiveTextureRendering()
-		{
-			glActiveTexture(GL_TEXTURE0);
-			glBindTexture(GL_TEXTURE_2D, object_texture_->get_texture_id());
-		}
+	}
 
-		void VisibleObject::SetObjectTransform(ObjTransInform target_transform)
-		{
-			object_transform_->set_position(target_transform.position);
-			object_transform_->set_rotation(target_transform.rotation);
-			object_transform_->set_scale(target_transform.scale);
-		}
-		void VisibleObject::SetObjectPosition(glm::vec3 target_position)
-		{
-			object_transform_->set_position(target_position);
-		}
-		void VisibleObject::SetObjectRotation(glm::vec3 target_rotation)
-		{
-			object_transform_->set_rotation(target_rotation);
-		}
-		void VisibleObject::SetObjectScale(glm::vec3 target_scale)
-		{
-			object_transform_->set_scale(target_scale);
-		}
-		void VisibleObject::MoveObject(glm::vec3 position)
-		{
-			object_transform_->Move(position);
-		}
-		void VisibleObject::RotateObject(glm::vec3 rotation)
-		{
-			object_transform_->Rotate(rotation);
-		}
-		void VisibleObject::ResizeObject(glm::vec3 scale)
-		{
-			object_transform_->Resize(scale);
-		}
-
-		void VisibleObject::SendProjectionAndViewMatrixToShader(glm::mat4 projection, glm::mat4 view)
-		{
-			object_shader_->SetMatrix4TypeUniformVariable("projection", projection);
-			object_shader_->SetMatrix4TypeUniformVariable("view", view);
-		}
-
-		void VisibleObject::Render()
-		{
-			object_shader_->SetMatrix4TypeUniformVariable("model", object_transform_->get_model_matrix());
-
-			glBindVertexArray(object_mesh_->get_vertex_array_object_identity());
-			glDrawArrays(GL_TRIANGLES, 0, object_mesh_->get_num_of_vertices());
-		}
-
-		void VisibleObject::SetTextureActive()
-		{
-			object_texture_->SetActive();
-		}
-
-		void VisibleObject::SetTransformActive()
-		{
-			object_transform_->SetActive();
-		}
-
-		void VisibleObject::SetMeshActive()
-		{
-			object_mesh_->SetActive();
-		}
-
-		void VisibleObject::SetTextureUnActive()
-		{
-			object_texture_->SetUnActive();
-		}
-
-		void VisibleObject::SetTransformUnActive()
-		{
-			object_transform_->SetUnActive();
-		}
-
-		void VisibleObject::SetMeshUnActive()
-		{
-			object_mesh_->SetUnActive();
-		}
-
-	}	// namespace object
-}	// namespace gdh_engine
+	void VisibleObject::Render() const
+	{
+		object_shader_->SetMatrix4TypeUniformVariable("model", object_transform_->get_model_matrix());
+		glBindVertexArray(object_mesh_->get_vertex_array_object_identity());
+		glDrawArrays(GL_TRIANGLES, 0, object_mesh_->get_num_of_vertices());
+	}
+	void VisibleObject::SendProjectionAndViewMatrixToShader
+	(glm::mat4 projection, glm::mat4 view)
+	{
+		object_shader_->SetMatrix4TypeUniformVariable("projection", projection);
+		object_shader_->SetMatrix4TypeUniformVariable("view", view);
+	}
+	void VisibleObject::SetTextureUniformToShader
+	(std::string tex_uniform_name, unsigned int tex_num) const
+	{
+		object_shader_->UseShader();
+		object_shader_->SetIntegerDataTypeUniformVariable(tex_uniform_name, tex_num);
+	}
+	void VisibleObject::SetTextureUniformToShader
+	(const char* tex_uniform_name, unsigned int tex_num) const
+	{
+		object_shader_->UseShader();
+		object_shader_->SetIntegerDataTypeUniformVariable(tex_uniform_name, tex_num);
+	}
+}	// namespace 
--- GongDolHoon/OpenGlSample/visible_object.h
+++ GongDolHoon/OpenGlSample/visible_object.h
@@ -3,76 +3,132 @@
 
 #include <string>
 #include <vector>
-
-// library
 #include "glm/glm.hpp"
 
-// custom library
 #include "interface_object.h"
+#include "loader_params.h"
 
-namespace gdh_engine {
-	namespace object {
-		class Texture;
-		class Shader;
-		class Mesh;
-		class Transform;
-		enum class TextureType;
+#pragma region COMPONENT_HEADER
+#include "transform.h"
+#include "mesh.h"
+#include "texture.h"
+#pragma endregion
 
-		typedef struct ObjectTransformInformation ObjTransInform;
+namespace object {
+	using namespace component;
+	class Shader;
 
-		class VisibleObject : public InterfaceObject
-		{
-		public:
-			// Constructer & Destructor
-			VisibleObject(const char* vertex_path, const char* fragment_path, const char* texture_path, 
-				const char* obj_path, ObjTransInform target_transform, 
-				TextureType dimensional_inform, TextureType alpha);
-			VisibleObject(std::string vertex_path, std::string fragment_path, std::string texture_path,
-				const char* obj_path, ObjTransInform target_transform,
-				TextureType dimensional_inform, TextureType alpha);
-			VisibleObject(const char* vertex_path, const char* fragment_path, const char* texture_path,
-				const char* obj_path, TextureType dimensional_inform, TextureType alpha);
-			VisibleObject(std::string vertex_path, const char* fragment_path, const char* texture_path,
-				const char* obj_path, TextureType dimensional_inform, TextureType alpha);
-			virtual ~VisibleObject();
+	class VisibleObject : public InterfaceObject
+	{
+	public:
+	#pragma region CONSTRUCTOR_DESTRUCTOR
+		VisibleObject(LoaderParams* params);
+		VisibleObject(const VisibleObject& other) = delete;
+		virtual ~VisibleObject();
+	#pragma endregion
+		// Overriding
+		virtual void Update() override;
 
-			// Overriding
-			virtual void Update() override;
+		inline void SetupMesh();
+		void SetTextureUniformToShader
+			(std::string tex_uniform_name, unsigned int tex_num) const;
+		void SetTextureUniformToShader
+			(const char* tex_uniform_name, unsigned int tex_num) const;
+		inline void ActiveTextureRendering() const;
 
-			void SetupMesh();
-			void SetTextureUniformToShader(std::string tex_uniform_name, unsigned int tex_num);
-			void SetTextureUniformToShader(const char* tex_uniform_name, unsigned int tex_num);
-			void ActiveTextureRendering();
-			void Render();
-			void SendProjectionAndViewMatrixToShader(glm::mat4 projection, glm::mat4 view);
+		void Render() const;
+		void SendProjectionAndViewMatrixToShader
+			(glm::mat4 projection, glm::mat4 view);
 
-			// About Transform
-			void SetObjectTransform(ObjTransInform target_transform);
-			void SetObjectPosition(glm::vec3 target_position);
-			void SetObjectRotation(glm::vec3 target_rotation);
-			void SetObjectScale(glm::vec3 target_scale);
-			void MoveObject(glm::vec3 position);
-			void RotateObject(glm::vec3 rotation);
-			void ResizeObject(glm::vec3 scale);
+		// About Transform
+		inline void SetObjectTransform(TransInform target_transform);
+		inline void SetObjectPosition(glm::vec3 target_position);
+		inline void SetObjectRotation(glm::vec3 target_rotation);
+		inline void SetObjectScale(glm::vec3 target_scale);
+		inline void MoveObject(glm::vec3 position);
+		inline void RotateObject(glm::vec3 rotation);
+		inline void ResizeObject(glm::vec3 scale);
 
-			// About Component Active
-			void SetTextureActive();
-			void SetTransformActive();
-			void SetMeshActive();
-			void SetTextureUnActive();
-			void SetTransformUnActive();
-			void SetMeshUnActive();
+		// About Component Active
+		inline void SetTextureActive();
+		inline void SetTransformActive();
+		inline void SetMeshActive();
+		inline void SetTextureUnActive();
+		inline void SetTransformUnActive();
+		inline void SetMeshUnActive();
 
-		private:
-			Texture* object_texture_;
-			Shader* object_shader_;
-			Mesh* object_mesh_;
-			Transform* object_transform_;
+	private:
+		Texture* object_texture_;
+		Shader* object_shader_;
+		Mesh* object_mesh_;
+		Transform* object_transform_;
 
-			glm::mat4 object_view_matrix_;
-			glm::mat4 object_projection_matrix_;
-		};
-	}	// namespace object
-}	// namespace gdh_engine
+		glm::mat4 object_view_matrix_;
+		glm::mat4 object_projection_matrix_;
+	};
+	void VisibleObject::SetupMesh()
+	{
+		object_mesh_->SetUpMesh();
+	}
+	void VisibleObject::SetObjectTransform(TransInform target_transform)
+	{
+		object_transform_->set_position(target_transform.position);
+		object_transform_->set_rotation(target_transform.rotation);
+		object_transform_->set_scale(target_transform.scale);
+	}
+	void VisibleObject::SetObjectPosition(glm::vec3 target_position)
+	{
+		object_transform_->set_position(target_position);
+	}
+	void VisibleObject::SetObjectRotation(glm::vec3 target_rotation)
+	{
+		object_transform_->set_rotation(target_rotation);
+	}
+	void VisibleObject::SetObjectScale(glm::vec3 target_scale)
+	{
+		object_transform_->set_scale(target_scale);
+	}
+	void VisibleObject::MoveObject(glm::vec3 position)
+	{
+		object_transform_->Move(position);
+	}
+	void VisibleObject::RotateObject(glm::vec3 rotation)
+	{
+		object_transform_->Rotate(rotation);
+	}
+	void VisibleObject::ResizeObject(glm::vec3 scale)
+	{
+		object_transform_->Resize(scale);
+	}
+	void VisibleObject::ActiveTextureRendering() const
+	{
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, object_texture_->get_texture_id());
+	}
+	void VisibleObject::SetTextureActive()
+	{
+		object_texture_->SetActive();
+	}
+	void VisibleObject::SetTransformActive()
+	{
+		object_transform_->SetActive();
+	}
+	void VisibleObject::SetMeshActive()
+	{
+		object_mesh_->SetActive();
+	}
+	void VisibleObject::SetTextureUnActive()
+	{
+		object_texture_->SetUnActive();
+	}
+	void VisibleObject::SetTransformUnActive()
+	{
+		object_transform_->SetUnActive();
+	}
+	void VisibleObject::SetMeshUnActive()
+	{
+		object_mesh_->SetUnActive();
+	}
+}	// namespace object
 
 #endif // GDH_ENGINE_VISIBLE_OBJECT_H
